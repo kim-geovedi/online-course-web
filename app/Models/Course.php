f878ff35183df9095338e60ffb670bc9b3c59c77
itlppan <it.lppan.id@gmail.com>
2024-07-07 03:13:41 +0700

    initialisasi project, membuat migrations controllers models, modifikasi migration dan models, mengatur mass assingment dan melakukan ORM ke setiap model serta membuat check subscription pada setiap user dan membuat views pages cms admin

---
+++ app/Models/Course.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Course extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'about',
+        'path_trailer',
+        'thumbnail',
+        'teacher_id',
+        'category_id'
+    ];
+    public function teacher(){
+        return $this->belongsTo(Teacher::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function course_videos(){
+        return $this->hasMany(CourseVideo::class);
+    }
+    public function course_keypoints(){
+        return $this->hasMany(CourseKeyPoint::class);
+    }
+    public function students(){
+        return $this->belongsToMany(User::class, 'course_students');
+    }
+}
